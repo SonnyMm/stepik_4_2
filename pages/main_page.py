8f552cbc8b8d3a36e187bf4c3b313cc7181e66ea
unknown <z.doloeva@gmail.com>
2024-11-24 02:18:51 +0500

    Добавлена обработка alert в метод go_to_login_page для корректной работы тестов

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,16 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        # Обрабатываем alert, если он появляется
+        try:
+            alert = self.browser.switch_to.alert
+            alert.accept()  # принимаем alert
+        except:
+            pass  # Если alert не появился, просто продолжаем выполнение
--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,7 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)  # Инициализация LoginPage
-    login_page.should_be_login_page()  # Проверка страницы логина
+    page.go_to_login_page()  # Метод теперь сам обрабатывает alert
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
